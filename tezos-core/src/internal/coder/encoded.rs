afcd372c0313a215ca79417f13059767b8948a6a
Mike Godenzi <m.godenzi@papers.ch>
2022-07-04 15:57:54 +0200

    chore(address-coder-refactoring): refactored contract_address_bytes_coder

--- tezos-core/src/internal/coder/encoded.rs
+++ tezos-core/src/internal/coder/encoded.rs
@@ -1,6 +1,5 @@
 pub mod address_bytes_coder;
 pub mod contract_address_bytes_coder;
-pub mod contract_hash_bytes_coder;
 pub mod encoded_bytes_coder;
 pub mod encoded_group_bytes_coder;
 pub mod implicit_address_bytes_coder;
