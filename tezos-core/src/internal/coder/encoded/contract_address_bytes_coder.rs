1aaadffa86291107a5de07e139ae5a5a42224d56
Mike Godenzi <m.godenzi@papers.ch>
2022-07-04 15:57:54 +0200

    chore(refactoring): refactored contract_address_bytes_coder

--- tezos-core/src/internal/coder/encoded/contract_address_bytes_coder.rs
+++ tezos-core/src/internal/coder/encoded/contract_address_bytes_coder.rs
@@ -1,21 +1,16 @@
 use crate::{
-    internal::coder::{ConfigurableDecoder, ConfigurableEncoder, Decoder, Encoder},
-    types::encoded::{ContractAddress, TraitMetaEncoded},
+    internal::coder::{Decoder, Encoder},
+    types::encoded::{ContractAddress, ContractHash, Encoded, TraitMetaEncoded},
     Error,
-};
-
-use super::contract_hash_bytes_coder::{
-    ContractHashBytesCoder, ContractHashBytesCoderConfiguration,
 };
 
 pub struct ContractAddressBytesCoder;
 
 impl Encoder<ContractAddress, Vec<u8>, Error> for ContractAddressBytesCoder {
     fn encode(value: &ContractAddress) -> std::result::Result<Vec<u8>, Error> {
-        let mut bytes = ContractHashBytesCoder::encode_with_configuration(
-            &value.contract_hash().try_into()?,
-            ContractHashBytesCoderConfiguration { with_suffix: true },
-        )?;
+        let contract_hash: ContractHash = value.contract_hash().try_into()?;
+        let mut bytes = contract_hash.to_bytes()?;
+        bytes.push(0);
         if let Some(entrypoint) = value.entrypoint() {
             bytes.extend_from_slice(entrypoint.as_bytes());
         }
@@ -27,10 +22,12 @@ impl Decoder<ContractAddress, [u8], Error> for ContractAddressBytesCoder {
     fn decode(value: &[u8]) -> std::result::Result<ContractAddress, Error> {
         let meta = ContractAddress::meta_value();
         let (contract_hash_bytes, entrypoint_bytes) = value.split_at(meta.bytes_length + 1);
-        let contract_hash = ContractHashBytesCoder::decode_with_configuration(
-            contract_hash_bytes.to_vec().as_ref(),
-            ContractHashBytesCoderConfiguration { with_suffix: true },
-        )?;
+        if !contract_hash_bytes.ends_with(&[0]) {
+            return Err(Error::InvalidBytes);
+        }
+        let contract_hash_end_index = contract_hash_bytes.len() - 1;
+        let contract_hash_bytes = &contract_hash_bytes[0..contract_hash_end_index];
+        let contract_hash = ContractHash::from_bytes(contract_hash_bytes)?;
         let entrypoint = if entrypoint_bytes.is_empty() {
             None
         } else {
